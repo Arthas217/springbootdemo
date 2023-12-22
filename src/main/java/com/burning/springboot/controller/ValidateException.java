b183f06ca8fcef9addc4320eb317b52cd9f48808
zuocheng <442854399@qq.com>
2023-12-22 10:37:07 +0800

    去掉无用注释

--- src/main/java/com/burning/springboot/controller/ValidateException.java
+++ src/main/java/com/burning/springboot/controller/ValidateException.java
@@ -2,10 +2,7 @@ package com.burning.springboot.controller;
 
 import com.burning.springboot.common.enums.ResponseStatus;
 import com.burning.springboot.response.Response;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindException;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -13,7 +10,7 @@ import java.util.Map;
 
 /**
  * @author 会游泳的蚂蚁
- * @description: 解决返回前端Whitelabel Error Page问题
+ * @description: 参考https://www.cnblogs.com/yanggb/p/10859907.html
  * @date 2023/12/19 18:37
  */
 @ControllerAdvice
